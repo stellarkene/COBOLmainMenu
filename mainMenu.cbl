7d82151bf74cf95160efbd753f327ddc504ee0c8
Sanchez <gabriel.raknchez@gmail.com>
2025-11-09 12:38:26 +0800

    sequence 1 - 5 functions and variables

--- mainMenu.cbl
+++ mainMenu.cbl
@@ -101,6 +101,7 @@
                WHEN 1
                    PERFORM CLEAR-SCREEN
                    DISPLAY "1 - PRINT NAME 5 TIMES"
+      *            PERFORM SEQ-1
 
                WHEN 2
                    PERFORM CLEAR-SCREEN
@@ -152,8 +153,106 @@
            END-PERFORM
            EXIT PARAGRAPH.
 
+      *SALES MAN INFORMATION
+       SEQ-5.
+           DISPLAY "PLEASE INPUT NAME: " WITH NO ADVANCING
+           ACCEPT SEQ5_SALES_MAN_NAME
 
+           DISPLAY "PLEASE INPUT NUM: " WITH NO ADVANCING
+           ACCEPT SEQ5_SALES_MAN_NUM
 
+           DISPLAY "PLEASE INPUT UNITS SOLD: " WITH NO ADVANCING
+           ACCEPT SEQ5_UNIT_SOLD
+
+           DISPLAY "PLEASE INPUT UNIT PRICE: " WITH NO ADVANCING
+           ACCEPT SEQ5_UNIT_PRICE
+
+           COMPUTE SEQ5_STORE_TOTAL_SALES = 
+-              SEQ5_UNIT_SOLD * SEQ5_UNIT_PRICE
+           DISPLAY "TOTAL SALES: " SEQ5_STORE_TOTAL_SALES
+
+           PERFORM EXIT-PROMPT
+           EXIT PARAGRAPH.
+
+      *CELCIUS TO FAHRENHEIT
+       SEQ-4.
+           
+           DISPLAY "PLEASE INPUT SEQ4_CELCIUS: " WITH NO ADVANCING
+           ACCEPT SEQ4_CELCIUS
+
+           COMPUTE SEQ4_FAHRENHEIT = (SEQ4_CELCIUS * 9 / 5) + 32
+           MOVE SEQ4_FAHRENHEIT TO SEQ4_F_OUT
+           DISPLAY "CONVERTED TO SEQ4_FAHRENHEIT: " SEQ4_F_OUT
+
+           PERFORM EXIT-PROMPT
+           EXIT PARAGRAPH.
+
+      *TWO NUMBER OPERATIONS
+       SEQ-3.
+           
+           MOVE 0 TO SEQ3_SolvedDisplay
+           MOVE 0 TO SEQ3_NumberA
+           MOVE 0 TO SEQ3_NumberB
+
+           DISPLAY "Please input for A: " WITH NO ADVANCING
+           ACCEPT SEQ3_NumberA
+           MOVE SEQ3_NumberA TO SEQ3_A_OUT
+
+           DISPLAY "Please input for B: " WITH NO ADVANCING
+           ACCEPT SEQ3_NumberB
+           MOVE SEQ3_NumberB TO SEQ3_B_OUT
+
+           DISPLAY "ADDITION: "
+           COMPUTE SEQ3_SolvedDisplay = SEQ3_NumberA + SEQ3_NumberB
+           DISPLAY " " SEQ3_A_OUT, " + ", " " SEQ3_B_OUT, " = "  
+-          SEQ3_SolvedDisplay
+
+           DISPLAY "SUBSTRACTION: "
+           COMPUTE SEQ3_SolvedDisplay = SEQ3_NumberA - SEQ3_NumberB
+           DISPLAY " " SEQ3_A_OUT, " - ", " " SEQ3_B_OUT, " = "  
+-          SEQ3_SolvedDisplay
+
+           DISPLAY "MULTIPLICATION: "
+           COMPUTE SEQ3_SolvedDisplay = SEQ3_NumberA * SEQ3_NumberB
+           DISPLAY " " SEQ3_A_OUT, " * ", " " SEQ3_B_OUT, " = "  
+-          SEQ3_SolvedDisplay
+
+           DISPLAY "DIVISION: "
+           COMPUTE SEQ3_SolvedDisplay = SEQ3_NumberA / SEQ3_NumberB
+           DISPLAY " " SEQ3_A_OUT, " / ", " " SEQ3_B_OUT, " = "  
+-          SEQ3_SolvedDisplay
+
+           PERFORM EXIT-PROMPT
+           EXIT PARAGRAPH.
+
+      *SWAP TWO NUMBERS
+       SEQ-2.
+           DISPLAY "BEFORE SWAP: ".
+           DISPLAY "A: " SEQ2_numA.
+           DISPLAY "B: " SEQ2_numB.
+ 
+           MOVE SEQ2_numB TO SEQ2_numC.
+           MOVE SEQ2_numA TO SEQ2_numB.
+           MOVE SEQ2_numC TO SEQ2_numA. 
+ 
+           DISPLAY "AFTER SWAP: ".
+           DISPLAY "A: " SEQ2_numA.
+           DISPLAY "B: " SEQ2_numB.
+           PERFORM EXIT-PROMPT
+           EXIT PARAGRAPH.
+
+      *PRINT NAME 5X
+       SEQ-1.
+
+           DISPLAY "GABRIEL E. SANCHEZ"
+           DISPLAY "GABRIEL E. SANCHEZ"
+           DISPLAY "GABRIEL E. SANCHEZ"
+           DISPLAY "GABRIEL E. SANCHEZ"
+           DISPLAY "GABRIEL E. SANCHEZ"
+
+           PERFORM EXIT-PROMPT
+           EXIT PARAGRAPH.
+       
       *CLEAR SCREEN FUNCTION
        CLEAR-SCREEN.
            ACCEPT OS-NAME FROM ENVIRONMENT "OS"
---
+++ mainMenu.cpy
@@ -0,0 +1,23 @@
+      *SEQ_2
+       01  SEQ2_numA                   PIC Z(2) VALUE 10.
+       01  SEQ2_numB                   PIC Z(2) VALUE 5.
+       01  SEQ2_numC                   PIC Z(2).
+
+      *SEQ_3
+       01  SEQ3_NumberA                PIC 999999V99.
+       01  SEQ3_A_OUT                  PIC ZZZ,ZZZ.ZZ.
+       01  SEQ3_NumberB                PIC 999999V99.
+       01  SEQ3_B_OUT                  PIC ZZZ,ZZZ.ZZ.
+       01  SEQ3_SolvedDisplay          PIC ZZZ,ZZZ.99.
+
+      *SEQ_4
+       01  SEQ4_CELCIUS                PIC S9(5)v99.
+       01  SEQ4_FAHRENHEIT             PIC S9(5)v99.
+       01  SEQ4_F_OUT                  PIC ZZ,ZZZ.99.
+
+      *SEQ_5
+       01  SEQ5_SALES_MAN_NUM          PIC 9(12).
+       01  SEQ5_SALES_MAN_NAME         PIC X(25).
+       01  SEQ5_UNIT_SOLD              PIC 9(5).
+       01  SEQ5_UNIT_PRICE             PIC 9(5)v99.
+       01  SEQ5_STORE_TOTAL_SALES      PIC $ZZ,ZZ9.
